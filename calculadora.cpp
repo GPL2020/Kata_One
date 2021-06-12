c2927a94f94aa1afd6174a30e097db0f80ebf9ee
GPL2020 <gplatta@gmail.com>
2021-06-12 03:30:11 -0300

    change .cpp to version 1.1

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,38 +1,52 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-int menu();
 double sumar(double x, double y);
 double restar(double x, double y);
 double multiplicar(double x, double y);
-double dividir(double x, double y);
+double dividir(double x, double y, bool &ok);
 
 int main(int argc, char *argv[]) {
 
     int opcion;
-    double resultado;
+    bool ok = true;
+    double resultado = 0;
 
-    
-    
-    cout << "Ingrese ENTER para finalizar... ";
-    cin.ignore();
-    cin.get();
+    if (argc >= 3) { // Al menos se tienen que pasar tres argumentos
+        // La pos 0 es el nombre del archivo ejecutable
+        double n1 = atof(argv[1]);  // argumento 1 - Primer operando
+        char operador = *argv[2];   // argumento 2 - Operador
+        double n2 = atof(argv[3]);  // argumento 3 - Segundo operando
+        
+        switch (operador) {
+            case '+':
+                resultado = sumar(n1, n2);
+                break;
+            case '-':
+                resultado = restar(n1, n2);
+                break;
+            case 'x':
+                resultado = multiplicar(n1, n2);
+                break;
+            case '/':
+                resultado = dividir(n1, n2, ok);
+                break;
+        }
 
+        if (ok) 
+            cout << "= " << resultado << endl;
+
+    }
+    else {
+        cout << "- Argumentos - " << endl
+             << "1 -> Operando 1" << endl
+             << "2 -> Operador (+ - x /)" << endl
+             << "3 -> Operando 2" << endl;
+    }
+    
     return 0;
-}
-
-int menu() {
-    int op;
-
-    cout << "1) Sumar" << endl
-         << "2) Restar" << endl
-         << "3) Multiplicar" << endl
-         << "4) Dividir" << endl
-         << "5) Salir" << endl;
-     cin >> op;
-
-    return op;
 }
 
 double sumar(double x, double y) {
@@ -47,12 +61,15 @@ double multiplicar(double x, double y) {
     return (x * y);
 }
 
-double dividir(double x, double y) {
-    double result;
+double dividir(double x, double y, bool &ok) {
+    double result = 0;
 
-    if (y != 0)
+    if (y != 0) {
         result = x / y;
+        ok = true;
+    }
     else 
-        result = 0;
+        ok = false;
+        
     return result;
 }
